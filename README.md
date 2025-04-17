04563ca6144d5f784f8687bfb2ac6bb15e4c1a90
ajsalmons <ajs515@case.edu>
2025-04-16 22:50:41 -0400

    Finished knobs and time domain plotter

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ECSE351_GNU_Problem_Set
- 
+    Alden Salmons
+    April 18th, 2025
+
+# Problem: Baba Yaga's Hut Simulation
+- User can vary amplitude, frequency, and phase
+- See time domain and phasor views
+
+Create a block diagram with a GUI that simulates the Phase Lab. In the GUI, the user should be able
+to vary the amplitude, frequency and phase of a signal, and see the time domain and phasor view in real
+time. (Consider the QT GUI Vector Sink block.) An excellent version will be used in future curriculum,
+with acknowledgment of your work.
+
+# Approach
+- Simulated the amplitude modulation hardware from the lab
+
+# Difficulties
+- After adjusting the phase, 
+
+# References
+1. GNU Radio Wiki https://wiki.gnuradio.org/index.php/Main_Page
+2. Chat GPT (https://chatgpt.com/)
+    1. Advice on color selection
+    2. Troubleshooting stream to vector block
+    3. Advice to use constellation block for XY plot
--- untitled.py
+++ untitled.py
@@ -10,6 +10,8 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
+from gnuradio import analog
+from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -19,6 +21,8 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import math
+import sip
 import threading
 
 
@@ -59,7 +63,7 @@ class untitled(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 10000
         self.p_y2 = p_y2 = 0
         self.p_y1 = p_y1 = 0
         self.f_y2 = f_y2 = 0.5
@@ -76,28 +80,28 @@ class untitled(gr.top_block, Qt.QWidget):
         	scaleFactor = 1
         else:
         	isFloat = True
-        	scaleFactor = 10
+        	scaleFactor = 5
 
-        _p_y2_dial_control = qtgui.GrDialControl('Phase (Y2)', self, 0,36,0,"purple",self.set_p_y2,isFloat, scaleFactor, 100, True, "'value'")
+        _p_y2_dial_control = qtgui.GrDialControl('Phase (Y2)', self, 0,72,0,"purple",self.set_p_y2,isFloat, scaleFactor, 100, True, "'value'")
         self.p_y2 = _p_y2_dial_control
 
-        self.top_grid_layout.addWidget(_p_y2_dial_control, 1, 5, 1, 1)
-        for r in range(1, 2):
+        self.top_grid_layout.addWidget(_p_y2_dial_control, 4, 6, 1, 1)
+        for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(5, 6):
+        for c in range(6, 7):
             self.top_grid_layout.setColumnStretch(c, 1)
         if "real" == "int":
         	isFloat = False
         	scaleFactor = 1
         else:
         	isFloat = True
-        	scaleFactor = 10
+        	scaleFactor = 5
 
-        _p_y1_dial_control = qtgui.GrDialControl('Phase (Y1)', self, 0,36,0,"orange",self.set_p_y1,isFloat, scaleFactor, 100, True, "'value'")
+        _p_y1_dial_control = qtgui.GrDialControl('Phase (Y1)', self, 0,72,0,"aqua",self.set_p_y1,isFloat, scaleFactor, 100, True, "'value'")
         self.p_y1 = _p_y1_dial_control
 
-        self.top_grid_layout.addWidget(_p_y1_dial_control, 1, 2, 1, 1)
-        for r in range(1, 2):
+        self.top_grid_layout.addWidget(_p_y1_dial_control, 4, 2, 1, 1)
+        for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(2, 3):
             self.top_grid_layout.setColumnStretch(c, 1)
@@ -108,13 +112,13 @@ class untitled(gr.top_block, Qt.QWidget):
         	isFloat = True
         	scaleFactor = 0.1
 
-        _f_y2_dial_control = qtgui.GrDialControl('Frequency (Y2)', self, 0,40,0.5,"purple",self.set_f_y2,isFloat, scaleFactor, 100, True, "'value'")
+        _f_y2_dial_control = qtgui.GrDialControl('Frequency (Y2)', self, 1,40,0.5,"purple",self.set_f_y2,isFloat, scaleFactor, 100, True, "'value'")
         self.f_y2 = _f_y2_dial_control
 
-        self.top_grid_layout.addWidget(_f_y2_dial_control, 1, 4, 1, 1)
-        for r in range(1, 2):
+        self.top_grid_layout.addWidget(_f_y2_dial_control, 4, 5, 1, 1)
+        for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(4, 5):
+        for c in range(5, 6):
             self.top_grid_layout.setColumnStretch(c, 1)
         if "real" == "int":
         	isFloat = False
@@ -123,11 +127,11 @@ class untitled(gr.top_block, Qt.QWidget):
         	isFloat = True
         	scaleFactor = 0.1
 
-        _f_y1_dial_control = qtgui.GrDialControl('Frequency (Y1)', self, 0,40,0.5,"orange",self.set_f_y1,isFloat, scaleFactor, 100, True, "'value'")
+        _f_y1_dial_control = qtgui.GrDialControl('Frequency (Y1)', self, 1,40,0.5,"aqua",self.set_f_y1,isFloat, scaleFactor, 100, True, "'value'")
         self.f_y1 = _f_y1_dial_control
 
-        self.top_grid_layout.addWidget(_f_y1_dial_control, 1, 1, 1, 1)
-        for r in range(1, 2):
+        self.top_grid_layout.addWidget(_f_y1_dial_control, 4, 1, 1, 1)
+        for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(1, 2):
             self.top_grid_layout.setColumnStretch(c, 1)
@@ -141,10 +145,10 @@ class untitled(gr.top_block, Qt.QWidget):
         _a_y2_dial_control = qtgui.GrDialControl('Amplitude (Y2)', self, 0,100,1,"purple",self.set_a_y2,isFloat, scaleFactor, 100, True, "'value'")
         self.a_y2 = _a_y2_dial_control
 
-        self.top_grid_layout.addWidget(_a_y2_dial_control, 1, 3, 1, 1)
-        for r in range(1, 2):
+        self.top_grid_layout.addWidget(_a_y2_dial_control, 4, 4, 1, 1)
+        for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(3, 4):
+        for c in range(4, 5):
             self.top_grid_layout.setColumnStretch(c, 1)
         if "real" == "int":
         	isFloat = False
@@ -153,15 +157,79 @@ class untitled(gr.top_block, Qt.QWidget):
         	isFloat = True
         	scaleFactor = 0.01
 
-        _a_y1_dial_control = qtgui.GrDialControl('Amplitude (Y1)', self, 0,100,1,"orange",self.set_a_y1,isFloat, scaleFactor, 100, True, "'value'")
+        _a_y1_dial_control = qtgui.GrDialControl('Amplitude (Y1)', self, 0,100,1,"aqua",self.set_a_y1,isFloat, scaleFactor, 100, True, "'value'")
         self.a_y1 = _a_y1_dial_control
 
-        self.top_grid_layout.addWidget(_a_y1_dial_control, 1, 0, 1, 1)
-        for r in range(1, 2):
+        self.top_grid_layout.addWidget(_a_y1_dial_control, 4, 0, 1, 1)
+        for r in range(4, 5):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(0, 1):
             self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            (samp_rate*4), #size
+            samp_rate, #samp_rate
+            'Time Series', #name
+            3, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
 
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(False)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_AUTO, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Y1', 'Y2', 'Y1×Y2', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [3, 3, 3, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['cyan', 'magenta', 'black', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 3, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(3):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 0, 0, 3, 4)
+        for r in range(0, 3):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 4):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
+        self.analog_sig_source_x_0_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, f_y1, a_y1, 0, (p_y1 * math.pi /180))
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, f_y2, a_y2, 0, (p_y2 * math.pi /180))
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_time_sink_x_0, 2))
 
 
     def closeEvent(self, event):
@@ -177,42 +245,51 @@ class untitled(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_p_y2(self):
         return self.p_y2
 
     def set_p_y2(self, p_y2):
         self.p_y2 = p_y2
+        self.analog_sig_source_x_0.set_phase((self.p_y2 * math.pi /180))
 
     def get_p_y1(self):
         return self.p_y1
 
     def set_p_y1(self, p_y1):
         self.p_y1 = p_y1
+        self.analog_sig_source_x_0_0.set_phase((self.p_y1 * math.pi /180))
 
     def get_f_y2(self):
         return self.f_y2
 
     def set_f_y2(self, f_y2):
         self.f_y2 = f_y2
+        self.analog_sig_source_x_0.set_frequency(self.f_y2)
 
     def get_f_y1(self):
         return self.f_y1
 
     def set_f_y1(self, f_y1):
         self.f_y1 = f_y1
+        self.analog_sig_source_x_0_0.set_frequency(self.f_y1)
 
     def get_a_y2(self):
         return self.a_y2
 
     def set_a_y2(self, a_y2):
         self.a_y2 = a_y2
+        self.analog_sig_source_x_0.set_amplitude(self.a_y2)
 
     def get_a_y1(self):
         return self.a_y1
 
     def set_a_y1(self, a_y1):
         self.a_y1 = a_y1
+        self.analog_sig_source_x_0_0.set_amplitude(self.a_y1)
 
 
 
